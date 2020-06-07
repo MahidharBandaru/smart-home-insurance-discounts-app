e11f742e2022f8a91d4f4e099a65731b6539e0ef
anjalisoni3655 <anjalisony32@gmail.com>
2020-06-08 00:36:37 +0530

    added env variable

--- home_insurance_app/lib/main.dart
+++ home_insurance_app/lib/main.dart
@@ -1,9 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:homeinsuranceapp/pages/home.dart';
 import 'package:homeinsuranceapp/welcome_screen.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_config/flutter_config.dart';
+
 
 void main() {
-  runApp(MyApp());
+  Future main() async {
+    await FlutterConfig.loadEnvVariables();
+   //await DotEnv().load('constant.env');
+    //...runapp
+    runApp(MyApp());
+  }
+
 }
 
 class MyApp extends StatelessWidget {
--- home_insurance_app/lib/welcome_screen.dart
+++ home_insurance_app/lib/welcome_screen.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:google_sign_in/google_sign_in.dart';
 import 'package:http/http.dart' as http;
+//import 'constant.env';
+import 'package:flutter_config/flutter_config.dart';
+
 import 'dart:io';
+//import '.env.dart';
 import 'dart:convert';
 import 'package:url_launcher/url_launcher.dart';
 import 'package:googleapis_auth/auth_io.dart';
 import 'package:homeinsuranceapp/components/rounded_button.dart';
 import 'package:animated_text_kit/animated_text_kit.dart';
-import 'package:homeinsuranceapp/constants.dart';
+//import 'package:homeinsuranceapp/constants.dart';
 
 GoogleSignIn _googleSignIn = GoogleSignIn(scopes: ['profile', 'email']);
-Class id = new Class();
+//Class id = new Class();
 class WelcomeScreen extends StatefulWidget {
   static const String id = 'welcome_screen';
 
@@ -113,21 +118,31 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
     _googleSignIn.disconnect();
   }
   Future<void> auth ( )async{
+//final String _clientId= DotEnv().env['CLIENT_ID'];
+//final String  _clientSecret=  DotEnv().env['CLIENT_SECRET'];
+//final String _url = DotEnv().env['CLIENT_URL'];
+//final String _url2= DotEnv().env['CLIENT_URL2'];
+//final String _scope = DotEnv().env['CLIENT_SCOPE'];
+final String _clientId= FlutterConfig.get('CLIENT_ID') ;
+final String  _clientSecret=  FlutterConfig.get('CLIENT_SECRET') ;
+final String _url = FlutterConfig.get('URL') ;
+final String _url2= FlutterConfig.get('URL2') ;
+final String _scope =FlutterConfig.get('SCOPE');
 
-    String _clientId= id.clientId;
-    String _clientSecret= id.clientSecret;
-    String _url = id.url;
-    String _url2 =id.url2;
-    List<String> _scopes = id.scopes;
+//final String _clientId=  "482460250779-8mk7lco5njbim010jimu5u1c39iisod7.apps.googleusercontent.com";
+     //final String _clientSecret= "-RSvp0BZvHjc6Pk05I_DpFeO";
+   //final  String _url =  "https://accounts.google.com/o/oauth2/auth?client_id=482460250779-8mk7lco5njbim010jimu5u1c39iisod7.apps.googleusercontent.com&redirect_uri=urn%3Aietf%3Awg%3Aoauth%3A2.0%3Aoob&response_type=code&scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fsdm.service&state=state";
+    //final String _url2 =
+        //'https://staging-smartdevicemanagement.sandbox.googleapis.com/v1/enterprises/akashag-step-interns-test/structures';
+    List<String> _scopes = [_scope];
     Uri.parse(_url);
-
 
     var authClient = await clientViaUserConsent(
         ClientId(_clientId, _clientSecret), _scopes, (url) {
       launch(url);
     });
 
-    String a = authClient.credentials.accessToken.data;
+    final String a = authClient.credentials.accessToken.data;
     print("Access Token");
     print(a);
 
