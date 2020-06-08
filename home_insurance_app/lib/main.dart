fe9fc6af2201c478fad5c4f801a549c11573eadf
anjalisoni3655 <anjalisony32@gmail.com>
2020-06-08 09:24:29 +0530

    used enviornnment variables

--- home_insurance_app/lib/main.dart
+++ home_insurance_app/lib/main.dart
@@ -3,19 +3,27 @@ import 'package:homeinsuranceapp/pages/home.dart';
 import 'package:homeinsuranceapp/welcome_screen.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_config/flutter_config.dart';
-void main() {
-  Future main() async {
+
+
+  void main() async {
+    WidgetsFlutterBinding.ensureInitialized();
+
     await FlutterConfig.loadEnvVariables();
-   //await DotEnv().load('constant.env');
-    //...runapp
+
     runApp(MyApp());
   }
 
-}
+
+
+
 
 class MyApp extends StatelessWidget {
   @override
+
+
   Widget build(BuildContext context) {
+    print(1);
+    print(FlutterConfig.get('URL2'));
     return MaterialApp(
       initialRoute: WelcomeScreen.id,
       routes: {
